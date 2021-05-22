95a70c12e9a526c9e52b18cea984ff9de4a6e48c
levankhelo <levankhelo@gmail.com>
2021-05-23 02:20:57 +0400

    fixed runner file for macos

--- macos-run.sh
+++ macos-run.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
+# Install XQuartz
 if ! command -v xquartz > /dev/null; then 
     brew install xquartz;  
 else 
     echo "Xquartz already installed"; 
 fi
 
+# Install SOCAT
 if ! command -v socat > /dev/null; then 
     brew install socat; 
 else 
     echo "SOCAT already installed"; 
 fi
 
-# kill SOCAT if running
+# Kill SOCAT if running
 kill -9 $(ps aux | grep socat | awk {'print $2'}); 
+
 # Start SOCAT
 osascript -e 'tell application "Terminal" to do script "socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\\\"$DISPLAY\\\"" '
 
+# Store IP address of device
 export ROBOCODE_LOCAL_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 
+# Build and run application
 docker-compose up --build
