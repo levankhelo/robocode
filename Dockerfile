39d179b44e2acd05260b112164af089c9056d4cb
levankhelo <levankhelo@gmail.com>
2021-05-23 01:40:20 +0400

    added gitkeep

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM ubuntu
-RUN apt-get update
-RUN apt-get install -y robocode
-RUN ln /usr/games/robocode /bin/robocode
-CMD robocode
--- macos-run.sh
+++ macos-run.sh
@@ -4,8 +4,8 @@ if ! command -v xquartz > /dev/null; then brew install xquartz;  else echo "Xqua
 
 if ! command -v socat > /dev/null; then brew install socat; else echo "SOCAT already installed"; fi
 
-osascript -e 'tell application "Terminal" to do script "socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\""'
+osascript -e 'tell application "Terminal" to do script "socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\\\"$DISPLAY\\\""'
 
-export ROBOCODE-LOCAL-IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+export ROBOCODE_LOCAL_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 
 docker-compose up --build
