1e68e87c264a7696bef10e7f72b654076f4cf764
levankhelo <levankhelo@gmail.com>
2021-05-27 12:04:10 +0400

    updated script for macos execution

--- macos-run.sh
+++ macos-run.sh
@@ -2,13 +2,23 @@
 
 # Install XQuartz
 if ! command -v xquartz > /dev/null; then 
+    echo "Installing Xquartz"; 
     brew install xquartz;  
 else 
     echo "Xquartz already installed"; 
 fi
+# Start XQuartz
+if [[ $(ps aux | grep XQuartz | awk {'print $2'} | wc -l) -gt 1 ]]; then 
+    echo "Xquartz already running"; 
+else
+    osascript -e 'tell application "Terminal" to do script "xquartz"';
+    sleep 10;
+fi
+
 
 # Install SOCAT
 if ! command -v socat > /dev/null; then 
+    echo "Installing SOCAT";
     brew install socat; 
 else 
     echo "SOCAT already installed"; 
@@ -21,7 +31,8 @@ kill -9 $(ps aux | grep socat | awk {'print $2'}) > /dev/null;
 osascript -e 'tell application "Terminal" to do script "socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\\\"$DISPLAY\\\""';
 
 # Store IP address of device
-export ROBOCODE_LOCAL_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1');
+export ROBOCODE_LOCAL_IP=$(ifconfig en0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1');
+echo $ROBOCODE_LOCAL_IP
 
 # Build and run application
 docker-compose up --build;
